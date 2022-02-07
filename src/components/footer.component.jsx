5d6be66924b3bbe5db5daeaef4510b3ce04d5cae
Caner <kurucaner@gmail.com>
2022-02-07 15:34:16 -0500

    first commit

---
+++ src/components/footer.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer-task">
+            <span>Drag and drop to reorder list</span>
+            <div className="attribution">
+            Caner Kuru
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
